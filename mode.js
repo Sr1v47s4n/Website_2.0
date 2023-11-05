ebbe081b2d8d90af32115cf6168f227203ee3249
Sr1v47s4n <srivatsansk.dev@gmail.com>
2023-11-05 09:09:46 +0530

    Completed the final flaws

--- mode.js
+++ mode.js
@@ -1,44 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.getElementById("modeToggle");
+    const themeIcons = document.getElementById("color-icon");
 
-const btn = document.getElementById("modeToggle");
-const themeIcons = document.getElementById("color-icon");
-const currentTheme = localStorage.getItem("theme");
+    // Check if the user's color mode preference is stored in local storage
+    const userColorModePreference = localStorage.getItem("colorMode");
 
-if (currentTheme === "dark") {
-    setDarkMode();
-    console.log("dark mode");
-} else {
-    setLightMode(); // Set the initial theme to light mode
-    console.log("light mode");
-}
+    // Check the user's browser color mode (light or dark)
+    const browserColorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
 
-btn.addEventListener("click", function () {
-    setTheme();
-});
+    // Determine the initial theme based on user preference or browser color mode
+    const initialTheme = userColorModePreference || browserColorMode;
 
-function setTheme() {
-
-    let currentTheme = document.body.getAttribute("theme");
-
-    if (currentTheme === "dark") {
-        setLightMode();
-    } else {
+    if (initialTheme === "dark") {
         setDarkMode();
+        console.log("dark mode");
+    } else {
+        setLightMode();
+        console.log("light mode");
     }
-}
 
-function setDarkMode() {
-    document.body.setAttribute("theme", "dark");
-    localStorage.setItem("theme", "dark");
+    btn.addEventListener("click", function () {
+        setTheme();
+    });
 
+    function setTheme() {
+        let currentTheme = document.body.getAttribute("theme");
+        if (currentTheme === "dark") {
+            setLightMode();
+        } else {
+            setDarkMode();
+        }
+    }
 
-}
+    function setDarkMode() {
+        document.body.setAttribute("theme", "dark");
+        localStorage.setItem("theme", "dark");
+        localStorage.setItem("colorMode", "dark"); // Store the user's color mode preference
+    }
 
-function setLightMode() {
-    document.body.removeAttribute("theme");
-    localStorage.setItem("theme", "light");
-
-
-}
-
-
-
+    function setLightMode() {
+        document.body.removeAttribute("theme");
+        localStorage.setItem("theme", "light");
+        localStorage.setItem("colorMode", "light"); // Store the user's color mode preference
+    }
+})
